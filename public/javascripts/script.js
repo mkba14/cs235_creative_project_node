9e63f23b1704d4211b7699276bfd3f6c2b61a549
EC2 Default User <ec2-user@ip-172-31-24-200.ec2.internal>
2019-11-05 21:10:33 +0000

    vanilla vue

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,25 +1,13 @@
-function toTitleCase(str) {
-  return str.replace(
-    /([^\W_]+[^\s-]*) */g,
-    function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }
-  );
-}
-
-
 var app = new Vue({
   el: '#app',
   data: {
-    cities: [],
-    prefix: '',
-    loading: true,
+    
   },
   created() {
    
   },
   methods: {
-    fetchDND() {
+    fetchDeck() {
     },
   },
 });
