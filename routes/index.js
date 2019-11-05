a80f55eaa6ab47430ecb038c4412b1023f010ba0
aMakenzie Allen <allenmakenzie1427@gmail.com>
2019-11-05 21:28:18 +0000

    decks added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require('request');
 
 var router = express.Router();
-
+var port = 4210;
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
@@ -15,39 +15,23 @@ router.get('/cards/new', function(req, res, next) {
   request(data).pipe(res);
 });
 
-router.get('/cards', function(req, res, next) {
-  console.log("new deck retrieved", req.query.q);
-  var data = "https://deckofcardsapi.com/api/deck/" + req.query.q + "/draw/?count=1";
+router.get('/cards/draw', function(req, res, next) {
+  console.log("new deck retrieved", req.query.num);
+  var data = "https://deckofcardsapi.com/api/deck/" + req.query.id + "/draw/?count=" + req.query.num;
   console.log(data);
   request(data).pipe(res);
-
 });
 
-router.get('/dnd', function(req, res, next) {
-  // req comes from browser.
-  console.log("In DND route");
-
-  // q = ... if we had used something else, it would have changed?
-  console.log(req.query.q);
-
-  var dndRest = "https://api.open5e.com/" + req.query.q + "/?format=json"
-  //var dndRest = "http://dnd5eapi.co/api/" + req.query.q;
-  //"https://api.open5e.com/monsters/?format=json"
-  console.log(dndRest);
-  request(dndRest).pipe(res);
+router.get('/cards/shuffle', function(req, res, next){
+   var data = "https://deckofcardsapi.com/api/deck/" + req.query.id + "/shuffle/";
+   request(data).pipe(res);
 });
 
-router.get('/dnd/monsters', function(req, res, next) {
-  // req comes from browser.
-  console.log("In DND route");
+router.get('/cards/hand', function(req, res, next){
+  
+  var data = "https://deckofcardsapi.com/api/deck/" + req.query.id + "/pile/"+ req.query.pid + "/add/?cards=" + req.query.cds;
+  
 
-  // q = ... if we had used something else, it would have changed?
-  console.log(req.query.q);
-  //var dndRest = "http://dnd5eapi.co/api/monsters"
-  var dndRest = "https://api.open5e.com/monsters/?" + req.query.q + "&format=json"
-  //"https://api.open5e.com/monsters/?format=json"
-  console.log(dndRest);
-  request(dndRest).pipe(res);
 });
 
 module.exports = router;
