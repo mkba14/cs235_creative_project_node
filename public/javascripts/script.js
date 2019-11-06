05846b0cdaa36d5243463f7642512da0e91f29bb
aMakenzie Allen <allenmakenzie1427@gmail.com>
2019-11-06 21:38:41 +0000

    cards now stack upon one another as expected

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,10 +4,12 @@ var app = new Vue({
   data: {
     deckId: "",
     cards: "",
-    hand_1: [],
+    hand_1: [],// [],
     hand_2: [],
     p1: false,
     p2: false,
+    num_drawn: 0,
+    total: 0,
   },
   created() {
     this.fetchDeck();
@@ -19,6 +21,7 @@ var app = new Vue({
         this.cards = response.data;
         console.log(this.cards);
         this.deckId = this.cards.deck_id;
+        this.total = response.data.remaining;
       }
       catch(error)
       {
@@ -28,9 +31,33 @@ var app = new Vue({
     async cardGetDraw() {
       try {
         let response = await axios.get("/cards/draw?id=" + this.deckId);
-        this.hand_1.push(response.data);
-        console.log(response);
-        console.log(this.hand_1[0]['cards'][0].value);
+        //this.hand_1.push("data", response.data);
+        console.log("data", response.data);
+        console.log("cards", response.data.cards[0]);
+        
+        this.hand_1.push(response.data.cards[0]);
+        this.num_drawn = this.total - response.data.remaining;
+        console.log("total", this.num_drawn);
+        //console.log(this.hand_1[-1]['cards'][0].value);
+        console.log(this.hand_1);
+      }
+      catch (error)
+      {
+        console.log(error);
+      }
+    },
+    async cardGetDraw_hand2() {
+      try {
+        let response = await axios.get("/cards/draw?id=" + this.deckId);
+        //this.hand_1.push("data", response.data);
+        console.log("data", response.data);
+        console.log("cards", response.data.cards[0]);
+        
+        this.hand_2.push(response.data.cards[0]);
+        this.num_drawn = this.total - response.data.remaining;
+        console.log("total", this.num_drawn);
+        //console.log(this.hand_1[-1]['cards'][0].value);
+        console.log(this.hand_2);
       }
       catch (error)
       {
